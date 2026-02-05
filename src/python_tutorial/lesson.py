a6dee6d86d5629ff14385f33d3be58c8d5367955
RojinTemell <rojintemel02@gmail.com>
2026-02-05 13:41:03 +0300

    lesson1

---
+++ src/python_tutorial/lesson.py
@@ -0,0 +1,25 @@
+print('Python\'s is so easy') #escape operator
+# Variable --> you dont need to mention variable types(String, int,double ..) in Python
+
+x=7
+y="hey"
+x='python'
+print(x)
+
+#variable casting
+x=str(7)
+y=int(7)
+z=float(7)
+print(x)
+print(y)
+print(z)
+
+#variable get the type
+print(type(x))
+print(type(y))
+print(type(z))
+#variable
+# x,y,z="banana","apple","grape"
+# a=b=c="red"
+colors=["white","black","green"]
+a,b,c=colors
